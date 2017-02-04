b856a03f5df8d9c21a45964a0822e550c9f04fa5
Varun Kumar <heysadboy@gmail.com>
2017-02-05 02:16:22 +0530

    Nothing is happening

--- Firebase/app/src/main/java/sadboy/firebase/MainActivity.java
+++ Firebase/app/src/main/java/sadboy/firebase/MainActivity.java
@@ -2,12 +2,47 @@ package sadboy.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView title;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = (TextView)findViewById(R.id.title);
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+
+        mFirebaseDatabase = mFirebaseInstance.getReference("hey");
+        mFirebaseInstance.getReference("new_title").setValue("Realtime Database");
+        mFirebaseInstance.getReference("new_title").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String appTitle = dataSnapshot.getValue(String.class);
+                Log.e("Hey", appTitle);
+                title.setText(appTitle);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e("Hey", "Failed to read app title value.", error.toException());
+            }
+        });
+
+
     }
 }
