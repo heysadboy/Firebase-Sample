c7ebe08df7dd1d5fd359a55084805f100bd5e54f
Varun Kumar <heysadboy@gmail.com>
2017-02-06 02:36:28 +0530

    Firebase sample

--- Firebase/app/src/main/java/sadboy/firebase/MainActivity.java
+++ Firebase/app/src/main/java/sadboy/firebase/MainActivity.java
@@ -3,6 +3,9 @@ package sadboy.firebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,8 +17,9 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private TextView title;
+    private EditText etInput;
+    private Button btSend;
     private DatabaseReference mFirebaseDatabase;
-    private FirebaseDatabase mFirebaseInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +27,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         title = (TextView) findViewById(R.id.title);
+        etInput = (EditText) findViewById(R.id.et_input);
+        btSend = (Button) findViewById(R.id.bt_send);
 
-        mFirebaseInstance = FirebaseDatabase.getInstance();
-        mFirebaseDatabase = mFirebaseInstance.getReference();
+        mFirebaseDatabase = FirebaseDatabase.getInstance().getReference();
 
-        mFirebaseDatabase.child("new_title").setValue("Realtime Database");
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String valuetoSend = etInput.getText().toString();
+                mFirebaseDatabase.child("new_title").setValue(valuetoSend);
+            }
+        });
+
         mFirebaseDatabase.child("new_title").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String appTitle = dataSnapshot.getValue().toString();
-                Log.e("Hey", appTitle);
                 title.setText(appTitle);
             }
 
